405582482bee2c7895fe83a3143d6a82649caa23
marknigh70@gmail.com <marknig70@gmail.com>
2023-03-24 10:37:00 -0500

    Change UI in CalendarView.vue to display events.

--- client/src/components/CalendarView.vue
+++ client/src/components/CalendarView.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="section">
         <p class="title2 has-text-weight-bold"> Today's Appointment</p>
-        <p v-for="event in events">
-            {{ event.subject }}
-        </p>
+        <ul v-for="event in events">
+            <li>{{ event.subject }}</li>
+        </ul>
     </section>
 </template>
 
